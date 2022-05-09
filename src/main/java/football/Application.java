9ca00e1beaa907f1773aea07f4e32d3669600618
ryabson1 <ryabson1990@gmail.com>
2022-05-09 10:52:06 +0300

    create class SoloGame for modulating solo games create class GameCases for choose types of results

--- src/main/java/football/Application.java
+++ src/main/java/football/Application.java
@@ -1,9 +1,13 @@
 package football;
 
+import football.model.Team;
+import football.utils.SoloGame;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        Team team = new Team("Реал Мадрид");
+        SoloGame.createSoloGame(team, "Барселона");
     }
 
 }
